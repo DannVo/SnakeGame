f5745ec7d48c1448a0913bf3f60c3ee9e5add529
Danh Vo <23083591-DanhVo11@users.noreply.replit.com>
2024-09-15 13:43:42 +0000

    Initial commit

---
+++ replit.nix
@@ -0,0 +1,33 @@
+{ pkgs }: {
+  deps = [
+    pkgs.portmidi
+    pkgs.pkg-config
+    pkgs.libpng
+    pkgs.libjpeg
+    pkgs.freetype
+    pkgs.fontconfig
+    pkgs.SDL2_ttf
+    pkgs.SDL2_mixer
+    pkgs.SDL2_image
+    pkgs.SDL2
+    pkgs.python310Full
+    pkgs.replitPackages.prybar-python310
+    pkgs.replitPackages.stderred
+  ];
+  env = {
+    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      # Needed for pandas / numpy
+      pkgs.stdenv.cc.cc.lib
+      pkgs.zlib
+      # Needed for pygame
+      pkgs.glib
+      # Needed for matplotlib
+      pkgs.xorg.libX11
+    ];
+    PYTHONHOME = "${pkgs.python310Full}";
+    PYTHONBIN = "${pkgs.python310Full}/bin/python3.10";
+    LANG = "en_US.UTF-8";
+    STDERREDBIN = "${pkgs.replitPackages.stderred}/bin/stderred";
+    PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python310}/bin/prybar-python310";
+  };
+}
